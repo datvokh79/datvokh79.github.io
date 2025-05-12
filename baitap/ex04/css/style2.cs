0200b671cf9ac1a6ce540f0ecfd7d28c3095d049
TienDat <vtdvotiendat@gmail.com>
2025-05-12 09:40:24 +0700

    Create style2.cs

---
+++ baitap/ex04/css/style2.cs
@@ -0,0 +1,56 @@
+/* Tổng thể */
+body {
+    font-family: sans-serif;
+    margin: 20px;
+    background-color: #f4f4f8;
+    color: #2c3e50;
+}
+
+/* Header */
+.header {
+    background: linear-gradient(to right, #007acc, #66b3ff);
+    padding: 40px 30px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    color: white;
+}
+
+
+.header h1 {
+    margin: 0;
+    font-size: 2rem;
+}
+
+/* Menu */
+nav ul {
+    list-style: none;
+    padding: 0;
+    margin: 0;
+    display: flex;
+    gap: 20px;
+}
+
+nav li {
+    font-size: 18px;
+    font-weight: bold;
+}
+
+nav a {
+    text-decoration: none;
+    color: white;
+    transition: color 0.3s;
+}
+
+nav a:hover {
+    color: #ffeb3b;
+}
+
+/* Footer */
+.footer {
+    background: linear-gradient(to right, #007acc, #66b3ff);
+    color: white;
+    text-align: center;
+    padding: 20px;
+}
+
